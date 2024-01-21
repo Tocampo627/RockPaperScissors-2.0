224ce99a4db004e2bb7556a422e561ca75635e96
Tanya627 <tanyaocampo96@yahoo.com>
2024-01-20 21:09:54 -0600

    Added landing page title, and play buttont, no functionality

---
+++ src/rps/LandingPage.tsx
@@ -0,0 +1,16 @@
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      <div>
+        <h1 className="game-title">A Classic Game of Rock Paper Scissors</h1>
+      </div>
+      <div>
+        <span className="material-symbols-outlined play-icon">play_arrow</span>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
